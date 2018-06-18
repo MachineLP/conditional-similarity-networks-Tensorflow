4684d19207523c1610bf39048e734944acb2aded
刘鹏 <liupeng@liupengdeMacBook-Pro.local>
2018-06-18 17:52:57 +0800

    Initial commit

---
+++ csn_lib/utils.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+    Created on 2018 06.18
+    @author: liupeng
+    wechat: lp9628
+    blog: http://blog.csdn.net/u014365862/article/details/78422372
+    """
+import tensorflow as tf
+slim = tf.contrib.slim
+import numpy as np
+import cv2
+import os
+import random
+
+def input_placeholder3(height, width, num_classes):
+    X1 = tf.placeholder(tf.float32, [None, height, width, 3])
+    X2 = tf.placeholder(tf.float32, [None, height, width, 3])
+    X3 = tf.placeholder(tf.float32, [None, height, width, 3])
+    Y = tf.placeholder(tf.int64, [None, num_classes])
+    is_train = tf.placeholder(tf.bool)
+    keep_prob_fc = tf.placeholder(tf.float32)
+    return X1, X2, X3, Y, is_train, keep_prob_fc
+def triplet_loss(anchor, positive, negative, alpha=1.):
+    with tf.variable_scope('triplet_loss'):
+        pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), 1)
+        neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), 1)
+        
+        basic_loss = tf.add(tf.subtract(pos_dist,neg_dist), alpha)
+        loss = tf.reduce_mean(tf.maximum(basic_loss, 0.0), 0)
+    return loss
+def g_parameter(checkpoint_exclude_scopes):
+    exclusions = []
+    if checkpoint_exclude_scopes:
+        exclusions = [scope.strip() for scope in checkpoint_exclude_scopes.split(',')]
+    variables_to_restore = []
+    variables_to_train = []
+    for var in tf.global_variables():
+        excluded = False
+        for exclusion in exclusions:
+            if var.op.name.startswith(exclusion):
+                excluded = True
+                variables_to_train.append(var)
+                break
+        if not excluded:
+            variables_to_restore.append(var)
+    return variables_to_restore,variables_to_train
+
+def data_norm(img):
+    img = img / 255.0
+    img = img - 0.5
+    img = img * 2
+    return img
+
+def get_next_batch_from_path(image_path, pointer, height, width, batch_size=64, training=True):
+    batch_x0 = np.zeros([batch_size, height, width,3])
+    batch_x1 = np.zeros([batch_size, height, width,3])
+    batch_x2 = np.zeros([batch_size, height, width,3])
+    C = np.zeros([batch_size,1])
+    for i in range(batch_size):
+        img_pairs = image_path[i+pointer*batch_size]
+        image0 = cv2.imread(img_pairs[0])
+        image1 = cv2.imread(img_pairs[1])
+        image2 = cv2.imread(img_pairs[2])
+        #image0 = add_img_padding(image0)
+        #image1 = add_img_padding(image1)
+        #image2 = add_img_padding(image2)
+        image0 = cv2.resize(image0, (height, width))
+        image1 = cv2.resize(image1, (height, width))
+        image2 = cv2.resize(image2, (height, width))
+        #if training: 
+        #    # image = data_aug([image])[0]
+        #    image0 = data_aug(image0)
+        #    image1 = data_aug(image1)
+        #    image2 = data_aug(image2)
+        image0 = data_norm(image0)
+        image1 = data_norm(image1)
+        image2 = data_norm(image2)
+        batch_x0[i,:,:,:] = image0
+        batch_x1[i,:,:,:] = image1
+        batch_x2[i,:,:,:] = image2
+        C[i,:] = img_pairs[3]
+    return batch_x0,batch_x1,batch_x2,C
+
+
+
---
+++ train.py
@@ -0,0 +1,169 @@
+# coding=utf-8
+"""
+Created on 2018 06.18
+@author: liupeng
+"""
+
+import numpy as np
+import tensorflow as tf
+slim = tf.contrib.slim
+import cv2
+import os
+try:
+    from image_loader import TripletImageLoader
+except:
+    from csn_lib.image_loader import TripletImageLoader
+
+try:
+    from csn_net import ConditionalSimNet, NetArch
+except:
+    from csn_lib.csn_net import ConditionalSimNet, NetArch
+try:
+    import config
+except:
+    from csn_lib import config
+
+try:
+    from utils import input_placeholder3, triplet_loss, g_parameter, get_next_batch_from_path
+except:
+    from csn_lib.utils import input_placeholder3, triplet_loss, g_parameter, get_next_batch_from_path
+
+
+height = config.height
+width = config.width
+epoch = config.epoch
+num_classes = config.num_classes
+n_conditions = config.n_conditions
+embedding_size = config.embedding_size
+conditions = config.conditions
+batch_size = config.batch_size
+dropout_prob = config.dropout_prob
+checkpoint_exclude_scopes = config.checkpoint_exclude_scopes
+learning_r_decay = config.learning_r_decay
+learning_rate_base = config.learning_rate_base
+decay_rate = config.decay_rate
+checkpoint_path = config.checkpoint_path
+early_stop = config.early_stop
+EARLY_STOP_PATIENCE = config.EARLY_STOP_PATIENCE
+embed_loss = config.embed_loss
+mask_loss = config.mask_loss
+
+
+def train(train_data_trip, val_data_trip, test_data_trip):
+    train_n = len(train_data_trip)
+    # print ('pairs_data', train_data)
+    valid_n = len(val_data_trip)
+    # print ('pairs_data', valid_data)
+    test_n = len(test_data_trip)
+    # ---------------------------------------------------------------------------------#
+    X0,X1,X2, Y, is_train, keep_prob_fc = input_placeholder3(height, width, num_classes)
+    C = tf.placeholder(tf.int32, [None, None])
+
+    with tf.variable_scope("", reuse=tf.AUTO_REUSE) as scope:
+        _, net_vis_x = NetArch().arch_vgg16(X0, num_classes, keep_prob_fc, is_train)
+        _, net_vis_y = NetArch().arch_vgg16(X1, num_classes, keep_prob_fc, is_train)
+        _, net_vis_z = NetArch().arch_vgg16(X2, num_classes, keep_prob_fc, is_train)
+        with tf.variable_scope("Logits_csn"):
+            csn = ConditionalSimNet(n_conditions, embedding_size, learnedmask=True, prein=True)
+            embedded_x, masknorm_norm_x, embed_norm_x, tot_embed_norm_x = csn.forward(net_vis_x, C)
+            embedded_y, masknorm_norm_y, embed_norm_y, tot_embed_norm_y = csn.forward(net_vis_y, C)
+            embedded_z, masknorm_norm_z, embed_norm_z, tot_embed_norm_z = csn.forward(net_vis_z, C)
+            mask_norm = (masknorm_norm_x + masknorm_norm_y + masknorm_norm_z) / 3
+            embed_norm = (embed_norm_x + embed_norm_y + embed_norm_z) / 3
+            mask_embed_norm = (tot_embed_norm_x + tot_embed_norm_y + tot_embed_norm_z) / 3
+    
+    # print (mask_norm)
+    loss_triplet = triplet_loss(embedded_x, embedded_y,embedded_z)
+    loss_embedd = embed_norm / np.sqrt(batch_size)
+    loss_mask = mask_norm / batch_size
+    loss = loss_triplet + embed_loss * loss_embedd + mask_loss * loss_mask
+
+    variables_to_restore,variables_to_train = g_parameter(checkpoint_exclude_scopes)
+    global_step = tf.Variable(0, trainable=False)  
+    if learning_r_decay:
+        learning_rate = tf.train.exponential_decay(  
+            learning_rate_base,                     
+            global_step * batch_size,  
+            train_n,                
+            decay_rate,                       
+            staircase=True)  
+    else:
+        learning_rate = learning_rate_base
+    # l_v = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,"Logits_csn")
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    with tf.control_dependencies(update_ops):
+        optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss, global_step=global_step, var_list=tf.global_variables())
+    # print ('>>>>>>', tf.global_variables())
+    accuracy = tf.reduce_sum(loss)
+    # accuracy = model_accuracy(net0, Y, num_classes)
+    #------------------------------------------------------------------------------------#
+    sess = tf.Session()
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    saver2 = tf.train.Saver(tf.global_variables())
+    #if not train_all_layers:
+    saver_net = tf.train.Saver(variables_to_restore)
+    saver_net.restore(sess, checkpoint_path)
+    
+    
+    # early stopping
+    best_valid = np.inf
+    best_valid_epoch = 0
+
+
+    for epoch_i in range(epoch):
+        for batch_i in range(int(train_n/batch_size)):
+            images0,images1,images2,cc = get_next_batch_from_path(train_data_trip, batch_i, height, width, batch_size=batch_size, training=True)
+            # print (images0,images1,images2,cc)
+            los, _ = sess.run([loss,optimizer], feed_dict={X0: images0,X1: images1,X2: images2,C:cc,is_train:True, keep_prob_fc:dropout_prob})
+            print (los)
+            ck_path = os.path.join('model', 'model.ckpt')
+            saver2.save(sess, ck_path, global_step=batch_i, write_meta_graph=False)
+            if batch_i%20==0:
+                loss_, acc_ = sess.run([loss, accuracy], feed_dict={X0: images0,X1: images1,X2: images2, C:cc,is_train:False, keep_prob_fc:1.0})
+                print('Batch: {:>2}: Training loss: {:>3.5f}, Training accuracy: {:>3.5f}'.format(batch_i, loss_, acc_))
+
+            if batch_i%100==0:
+                images0,images1,images2,cc = get_next_batch_from_path(val_data_trip, batch_i%(int(valid_n/batch_size)), height, width, batch_size=batch_size, training=False)
+                ls, acc = sess.run([loss, accuracy], feed_dict={X0: images0,X1: images1,X2: images2,C:cc, is_train:False, keep_prob_fc:1.0})
+                print('Batch: {:>2}: Validation loss: {:>3.5f}, Validation accuracy: {:>3.5f}'.format(batch_i, ls, acc))
+            
+        
+        print('Epoch===================================>: {:>2}'.format(epoch_i))
+        valid_ls = 0
+        valid_acc = 0
+        for batch_i in range(int(valid_n/batch_size)):
+            images_valid0,images_valid1,images_valid2,cc = get_next_batch_from_path(val_data_trip, batch_i, height, width, batch_size=batch_size, training=False)
+            epoch_ls, epoch_acc = sess.run([loss, accuracy], feed_dict={X0:images_valid0,X1:images_valid1,X2:images_valid2, C:cc,keep_prob_fc:1.0, is_train:False})
+            valid_ls = valid_ls + epoch_ls
+            valid_acc = valid_acc + epoch_acc
+        print('Epoch: {:>2}: Validation loss: {:>3.5f}, Validation accuracy: {:>3.5f}'.format(epoch_i, valid_ls/int(valid_n/batch_size), valid_acc/int(valid_n/batch_size)))
+        
+        '''
+        if valid_acc/int(valid_n/batch_size) > 0.90:
+            ck_path = os.path.join('model', 'model.ckpt')
+            saver2.save(sess, ck_path, global_step=epoch_i, write_meta_graph=False)'''
+        
+        # ---------------------------------------------------------------------------------#
+        if early_stop:
+            loss_valid = valid_ls/int(valid_n/batch_size)
+            if loss_valid < best_valid:
+                best_valid = loss_valid
+                best_valid_epoch = epoch_i
+            elif best_valid_epoch + EARLY_STOP_PATIENCE < epoch_i:
+                print("Early stopping.")
+                print("Best valid loss was {:.6f} at epoch {}.".format(best_valid, best_valid_epoch))
+                break
+
+    sess.close()
+
+
+if __name__ == '__main__':
+    # 获取训练数据
+    train_data_trip = TripletImageLoader('data', 'ut-zap50k-images', 'filenames.json', conditions, 'train', n_triplets=100000).get_trip_data()
+    print (train_data_trip[:100])
+    val_data_trip = TripletImageLoader('data', 'ut-zap50k-images', 'filenames.json', conditions, 'val', n_triplets=80000).get_trip_data()
+    print (val_data_trip[:100])
+    test_data_trip = TripletImageLoader('data', 'ut-zap50k-images', 'filenames.json', conditions, 'test', n_triplets=160000).get_trip_data()
+    print (test_data_trip[:100])
+    train(train_data_trip, val_data_trip, test_data_trip)
