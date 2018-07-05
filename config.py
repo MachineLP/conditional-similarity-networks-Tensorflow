74569d5089c4a13dcdb51e8dc48da12d4c7be442
MachineLP <MachineLP@163.com>
2018-07-05 12:16:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -36,4 +36,4 @@ checkpoint_path = 'pretrain/vgg/vgg_16.ckpt'
 # checkpoint_path = 'pretrain/inception_v4/inception_v4.ckpt'
 # checkpoint_path = 'pretrain/resnet_v2/resnet_v2_50.ckpt'
 # 需要分类的类别数量,这个参数没用到
-num_classes = 10
+num_classes = 4
